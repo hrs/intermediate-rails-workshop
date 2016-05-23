d5036c2b05e6d30429a7d4a899671b4473b9b505
Harry Schwartz <hello@harryrschwartz.com>
2016-05-23 14:08:43 -0400

    Create Shouts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523163642) do
+ActiveRecord::Schema.define(version: 20160523180356) do
+
+  create_table "shouts", force: :cascade do |t|
+    t.string   "body",       null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "shouts", ["user_id"], name: "index_shouts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                        null: false
