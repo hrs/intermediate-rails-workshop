98a4ea8a0a84580d711e083e996dbc2706c4016b
Harry Schwartz <hello@harryrschwartz.com>
2016-05-23 16:43:58 -0400

    Validate that shouts aren't empty

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -5,8 +5,12 @@ class ShoutsController < ApplicationController
 
   def create
     shout = current_user.shouts.build(shout_params)
-    shout.save
-    redirect_to dashboard_path
+    if shout.save
+      redirect_to dashboard_path
+    else
+      flash.alert = "Couldn't shout that. =("
+      redirect_to dashboard_path
+    end
   end
 
   private
