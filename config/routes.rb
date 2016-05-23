48da47678c9931d146c51f14e3e82e582e7f59af
Harry Schwartz <hello@harryrschwartz.com>
2016-05-23 13:39:39 -0400

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "homes#show"
 
+  resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 end
